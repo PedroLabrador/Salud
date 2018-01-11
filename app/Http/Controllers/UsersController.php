e96fb883fa7f90ed09ee8b8b98a00ac054968742
PedroLabrador <pedrolabrador2@gmail.com>
2018-01-10 22:12:19 -0400

    Commented some lines to use Model public function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,10 +10,13 @@ class UsersController extends Controller
 {
     public function show($username) {
         $user = User::where('username', $username)->first();
-        $posts = Post::all()->where('user_id', $user->id);
+        //$posts = Post::all()->where('user_id', $user->id);
+
+        //Makes use of User model with a public function that return hasMany values for user posts
+        //$posts = $user->posts;
         return view('users.show', [
             'user' => $user,
-            'posts' => $posts,
+            //'posts' => $posts,
         ]);
     }
 }
