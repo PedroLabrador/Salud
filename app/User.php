b64e6d91caff5fac0bd9c16a2ee23a543b3b0141
PedroLabrador <pedrolabrador2@gmail.com>
2018-01-10 22:13:08 -0400

    Created public function that return hasMany relation

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -27,4 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts() {
+        return $this->hasMany(Post::class)->orderBy('created_at', 'desc');
+    }
 }
